3bd7869f14883b55944582a247cfa30c02428b2a
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-01-18 12:41:24 +0530

    create test route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import userRoutes from "./routes/user.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -9,6 +10,8 @@ mongoose
   .catch((err) => console.log("error connecting to database"));
 
 const app = express();
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log("server up!");
