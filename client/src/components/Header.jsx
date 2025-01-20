2eb0e993bc69c75bea052c608f1005a0eb23eb1d
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-01-20 11:35:25 +0530

    made profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
