cea8410a49a70c1e234575aa9b0116edd1a6c41a
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-01-20 09:33:53 +0530

    complete signup functionality

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ import authRoutes from "./routes/auth.route.js";
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("database connected"))
-  .catch((err) => console.log("error connecting to database"));
+  .catch((err) => console.log("error connecting to database", err));
 
 const app = express();
 
@@ -16,6 +16,16 @@ app.use(express.json());
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("server up!");
